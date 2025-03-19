10f48e72b692560482127858d3993332c403459e
theredenemy <1234corbin.1234ad@gmail.com>
2025-03-18 21:45:08 -0700

    modified:   configmenu.py 	new file:   requirements.txt

--- configmenu.py
+++ configmenu.py
@@ -1,6 +1,8 @@
 def makeinstallconfig():
     import configparser
     import os
+    import win32ui
+    import win32con
     gitrepodefault = "https://github.com/theredenemy/SteamDeckWindowsShell"
     installdirdefault = "C:/SteamDeckShell"
     print("Welcome Text")
@@ -10,5 +12,15 @@ def makeinstallconfig():
     installdir = input(f"Enter Installation Directory. default:{installdirdefault}: ")
     if not installdir:
         installdir = installdirdefault
+    #ask_uac_off = input("Turn off UAC. Recommended so there no UAC Prompt every login. default:y")
+    ask_uac = win32ui.MessageBox("Turn off UAC. Recommended so there no UAC Prompt every login.", "Steam Deck Windows Shell Installer", win32con.MB_YESNO)
+    if ask_uac == win32con.IDYES:
+        uacoff = True
+    elif ask_uac == win32con.IDNO:
+        uacoff = False
+    else:
+        uacoff = True
+    
+
     
     
